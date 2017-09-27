07eff974eb95758422a9caa04a2387fd58f0027c
martynek <xmartynsx@gmail.com>
2017-09-27 11:56:00 +0200

    Najwieksza ABC

--- cpp/najwiekszaABC.cpp
+++ cpp/najwiekszaABC.cpp
@@ -17,20 +17,43 @@ int main(int argc, char **argv)
     
     
     if (a > b) 
-    {
-      if (a > c)
+         {
+         if (a > c)
          cout << "Największe a=" << a;
-         else 
-          cout << "Największe c=" << c;
-    } 
+         else if (a < c)
+         cout << "Największe c=" << c;
+         else if (a == c)
+         cout << "Najwieksze a=c=" << a;
+         } 
+    
     else if (b > a)
-    {
-      if (b > c)
-         cout << "Największe b=" << b;
-         else
-         
-          cout << "Największe c=" << c;
-    }
+        {
+        if (b > c)
+        cout << "Największe b=" << b;
+        else if ( b < c)
+        cout << "Największe c=" << c;
+        else if (b == c)
+         cout << "Największe b=c=" << b;
+        }
+    
+    else if (c > a )
+        {
+        if (c > b)
+        cout << "Najwieksze c= " << c;
+        else if (b > c)
+        cout << "Najwieksze b=" << b;
+        else if (c == b)
+        cout << "Największe c=b=" << c;
+        }
+    else if (a == b)
+        {
+        if (c >= b)
+        cout << "Najwieksze a=c=b " << a;
+        else if (c < a )
+        cout << "Najwieksze a=b " << a;
+        else if (c > a )
+        cout << "Najwieksze c= " << c;
+        }
    
 	return 0;
 }
