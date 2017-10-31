7250d4017cc835ba83d031f525caf41cc0252cd4
martynek <xmartynsx@gmail.com>
2017-10-31 13:31:17 +0100

    dodaj, witaj

---
+++ python/witaj.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#osoba = "Adam Mickiewicz"
+#osoba = 'Adam Mickiewicz'
+
+osoba = input("Jak sie nazywasz?\n")
+print("Witaj", osoba, "!")
+
+rok = 2017
+wiek = int(input("Ile masz lat?\n"))
+print("Urodziles sie w", rok - wiek, "roku!")
