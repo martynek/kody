fbc398b2b83968dfb733828a16c77c9318a594a4
Your Name <xmartynsx@gmail.com>
2017-11-22 23:10:38 +0100

    minimax i euklides

---
+++ cpp/euklides.cpp
@@ -0,0 +1,33 @@
+/*
+ * euklides.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int euklides(int a, int b) {
+    while(a != b)
+    {
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+    return a;
+    }
+
+int main(int argc, char **argv)
+{
+    int a, b;
+    cout << "Podaj liczbę: ";
+    cin >> a;
+    cout << "Podaj liczbę: ";
+    cin >> b;
+
+    cout << "Największy wspólny dzielnik  =  " << euklides(a,b) << endl;
+    
+    return 0;
+}
+
---
+++ python/euklides.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  bez nazwy.py  
+#  
+
+def euklides(a, b):
+    while a != b:
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
+    return a
+
+def euklides2(a, b):
+    while a > 0:
+        a = a % b
+        b = b - a
+    return b
+
+def main(args):
+    a = int(input("Podaj liczbę: "))
+    b = int(input("Podaj liczbę: "))
+    assert euklides2(1989, 867) == 51
+    assert euklides2(12, 6) == 6
+    print("Nwd({:d}, {:d}) = {:d}".format(a, b, euklides2(a, b)))
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
