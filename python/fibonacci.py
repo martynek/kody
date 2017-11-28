f95d05e2089dd7da1c2aa5db460aef2fe522ce81
Your Name <xmartynsx@gmail.com>
2017-11-28 22:43:34 +0100

    Fibonacci

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -12,14 +12,15 @@ def fib_iter(n):
     """
     a, b = (0, 1)
     if n == 0:
-        print a
+        print("a")
         return a
     elif n == 1:
-        print b
+        print ("b")
         return b
 
-    for i in range(1, n):
-        pass
+    for i in range(1, n-1):
+        a, b = b, a + b
+        
 
     return b
 
