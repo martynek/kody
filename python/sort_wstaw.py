944006d5da956939496d0c9744b146242a20b296
martynek <xmartynsx@gmail.com>
2018-01-05 11:40:20 +0100

    binarnie

---
+++ python/sort_wstaw.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def sort_wstaw(lista):
+    """wersja liniowa"""
+    for i in range(1, len(lista)):
+        # for(int i=0; i < n; i++)
+        el = lista[i]
+        k = i - 1
+        while k >= 0 and lista[k] > el:  # wyszukiwanie pozycji
+            lista[k + 1] = lista[k]  # przesuwanie elementów
+            k -= 1
+        lista[k + 1] = el  # wstawianie elementu
+    return lista
+
+
+def wyszukaj_bin_it(lista, el):
+    lewy, prawy = 0, len(lista) - 1
+    while lewy < prawy:
+        srodek = floor((lewy + prawy) / 2)
+        # pprint(srodek)
+        if el <= lista[srodek]:
+            prawy = srodek
+        else:
+            lewy = srodek + 1
+    if lista[lewy] == el:
+        return lewy
+
+    return -1
+
+
+def sort_wstaw_bin(lista):
+    """wersja binarna"""
+    for i in range(1, len(lista)):
+        el = lista[i]
+        k = wyszukaj_bin_it(lewy, prawy, lista, el)
+        # tworzenie listy z wstawionym elementem
+        # todo
+        lista =
+
+
+def main(args):
+    lista = [4, 3, 7, 0, 2, 3, 1, 9, -6]
+    print(lista)
+    # [3, 4, 7, 0, 2, 3, 1, 9]
+    # [3, 4, 7, 0, 2, 3, 1, 9]
+    # [0, 3, 4, 7, 2, 3, 1, 9]
+    print(sort_wstaw(lista))
+
+
+return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
