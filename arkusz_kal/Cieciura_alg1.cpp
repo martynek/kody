d1a95ff107c339444f8bddd92445fe29adb72ac9
martynek <xmartynsx@gmail.com>
2018-01-23 13:27:39 +0100

    zlozonosc

---
+++ arkusz_kal/Cieciura_alg1.cpp
@@ -0,0 +1,36 @@
+/*
+ * Cieciura_alg1.cpp 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int a =0;
+    int i = 0;
+    
+    cout << "Podaj a" << endl;
+    cin >> a;
+    
+    if (a < 0 || a > 100)
+    {
+        cout << "Podaj a" << endl;
+        cin >> a;
+    }
+    else 
+    i = 2;
+    
+    for(int i = 0; i = a; i++)
+    
+    
+    
+    
+	
+
+    
+	return 0;
+}
+
+// algorytm nie jest poprawny
