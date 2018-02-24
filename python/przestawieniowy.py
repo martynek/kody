01cd89a0cf9c73c80dc21d53632904222a41e3cc
Your Name <xmartynsx@gmail.com>
2018-02-24 22:45:04 +0100

    szyfry

---
+++ python/przestawieniowy.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def szyfruj(tekst, klucz):
+    reszta = len(tekst) % klucz
+    if reszta:
+        tekst += (klucz - reszta) * "."
+        
+    szyfrogram = ""
+
+    for i in range(klucz):
+        for j in range(int(len(tekst) / klucz)):
+            print(i + j * klucz, tekst[i + j * klucz])
+            szyfrogram += tekst[i + j * klucz]
+    return szyfrogram
+    
+    
+def deszyfruj(szyfrogram, klucz):
+    tekst = ""
+    reszta = len(tekst) % klucz
+    
+    for i in range(klucz):
+        for j in range(int(len(szyfrogram) / klucz)):
+            print()
+            
+    return tekst
+
+
+def main(args):
+    tekst = input("Podaj tekst: ")
+    klucz = int(input("Podaj klucz: "))
+    while (2 * klucz > len(tekst)):
+        klucz = int(input("Podaj klucz: "))
+
+    szyfrogram = szyfruj(tekst, klucz)
+    print(szyfrogram)
+    # print(deszyfruj(szyfrogram, klucz))
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
---
+++ python/szyfr_cezara.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def szyfruj(tekst, klucz):
+    """Szyfrowanie tekstu za pomoca szyfru Cezara"""
+    szyfrogram = ""
+    klucz = klucz % 26
+    for znak in tekst:
+        if ord(znak)>64 and ord(znak)<91:
+            ascii = ord(znak) + klucz
+            if ascii > 90:
+                ascii -= 26
+         if ord(znak)>96 and ord(znak)<123:
+            ascii = ord(znak) + klucz
+        szyfrogram += chr(ascii)
+    return szyfrogram
+
+
+def deszyfruj(szyfrogram, klucz):
+    tekst = ""
+    klucz = klucz % 26
+    for znak in szyfrogram:
+        if ord(znak)>64 and ord(znak)<91:
+            ascii = ord(znak) - klucz
+            if ascii > 90:
+                ascii -= 26
+        if ord(znak)>96 and ord(znak)<123:
+            ascii = ord(znak) - klucz
+        tekst += chr(ascii)
+    return tekst
+
+#  obsłużyć małe i duże litery
+#  obsłużyć spacje
+
+
+def main(args):
+    tekst = input("Podaj tekst: ")
+    klucz = int(input("Podaj klucz: "))
+    szyfrogram = szyfruj(tekst, klucz)
+    print(szyfrogram)
+    print(deszyfruj(szyfrogram, klucz))
+
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
