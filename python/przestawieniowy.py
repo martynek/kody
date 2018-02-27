95402f787bb2e7177fa813ecb02eee9ae82f0617
Your Name <xmartynsx@gmail.com>
2018-02-27 20:38:01 +0100

    szyfry

--- python/przestawieniowy.py
+++ python/przestawieniowy.py
@@ -3,40 +3,47 @@
 
 
 def szyfruj(tekst, klucz):
+    szyfrogram = ""
     reszta = len(tekst) % klucz
+
     if reszta:
         tekst += (klucz - reszta) * "."
-        
-    szyfrogram = ""
 
-    for i in range(klucz):
-        for j in range(int(len(tekst) / klucz)):
-            print(i + j * klucz, tekst[i + j * klucz])
+    for i in range(klucz):  # 0-9
+        # print("Wydrukowane i: ", i)
+        for j in range(int(len(tekst) / klucz)):  # 0-3
+            # print("j: ", j)
             szyfrogram += tekst[i + j * klucz]
     return szyfrogram
-    
-    
+
+
 def deszyfruj(szyfrogram, klucz):
     tekst = ""
-    reszta = len(tekst) % klucz
-    
-    for i in range(klucz):
-        for j in range(int(len(szyfrogram) / klucz)):
-            print()
-            
+    for i in range(int(len(szyfrogram) / klucz)):  # 0-3
+        for j in range(klucz):  # 0-9
+            # print(j)
+            # print("i=", i, " + ", "klucz=", int(len(szyfrogram) / klucz), " * ", "j=", j, " rowna sie: ", i + (j * int(len(szyfrogram) / klucz)) )
+            tekst += szyfrogram[i + (j * int(len(szyfrogram) / klucz))]
+            # tekst = tekst.replace(".", "")
+
     return tekst
 
 
 def main(args):
     tekst = input("Podaj tekst: ")
+
     klucz = int(input("Podaj klucz: "))
-    while (2 * klucz > len(tekst)):
+    # while (2 * klucz > len(tekst)):
+    while klucz > (0.5 * len(tekst)):
         klucz = int(input("Podaj klucz: "))
 
     szyfrogram = szyfruj(tekst, klucz)
-    print(szyfrogram)
-    # print(deszyfruj(szyfrogram, klucz))
+    print("Zaszyfrowany: ", szyfrogram)
+    deszyfrowany = deszyfruj(szyfrogram, klucz)
+    print("Deszyfrowany: ", deszyfrowany)
+
     return 0
+
 
 if __name__ == '__main__':
     import sys
--- python/szyfr_cezara.py
+++ python/szyfr_cezara.py
@@ -4,40 +4,52 @@
 
 def szyfruj(tekst, klucz):
     """Szyfrowanie tekstu za pomoca szyfru Cezara"""
-    szyfrogram = ""
     klucz = klucz % 26
-    for znak in tekst:
-        if ord(znak)>64 and ord(znak)<91:
-            ascii = ord(znak) + klucz
-            if ascii > 90:
-                ascii -= 26
-         if ord(znak)>96 and ord(znak)<123:
-            ascii = ord(znak) + klucz
+    szyfrogram = ""
+    # znaki_pl = ["Ą", "Ć", "Ę", "Ł", "Ń", "Ó", "Ś", "Ź", "Ż", "ą", "ć", "ę",
+    #         "ł", "ń", "ó", "ś", "ź", "ż"]
+    for i in tekst:
+        ascii = ord(i) + klucz
+        if ord(i) == 32:
+            ascii = 32
+        if ascii > 90 and ascii < 97 or ascii > 122:
+            ascii -= 26
+        # elif ascii > 122:
+        #     ascii -= 26
+
         szyfrogram += chr(ascii)
+
     return szyfrogram
 
 
-def deszyfruj(szyfrogram, klucz):
+def deszyfruj(szyfrogram, klucz): # troche duzo ifow, ale narazie nwm jak to przyspieszyc i uproscic. wazne ze dziala
     tekst = ""
-    klucz = klucz % 26
-    for znak in szyfrogram:
-        if ord(znak)>64 and ord(znak)<91:
-            ascii = ord(znak) - klucz
-            if ascii > 90:
-                ascii -= 26
-        if ord(znak)>96 and ord(znak)<123:
-            ascii = ord(znak) - klucz
+    for i in szyfrogram:
+        ascii = ord(i) - klucz
+        # print(i, ascii)
+        if ord(i) == 32:
+            ascii = 32
+        if ascii < 65 and ascii != 32:
+            ascii += 26
+        if ascii > 90 and ascii < 97 and ascii != 32:
+            ascii += 26
+        if i.isdigit():
+            ascii -= 26
+        # print(i, ascii)
         tekst += chr(ascii)
+
     return tekst
 
-#  obsłużyć małe i duże litery
-#  obsłużyć spacje
+# oblsłuzyć male i duże litery
+# obsluzyc spacje
 
 
 def main(args):
     tekst = input("Podaj tekst: ")
     klucz = int(input("Podaj klucz: "))
     szyfrogram = szyfruj(tekst, klucz)
+    # szyfrogram = "VCBIU FHCDUD MHVW SURVWB"
+    # klucz = 3
     print(szyfrogram)
     print(deszyfruj(szyfrogram, klucz))
 
