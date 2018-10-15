e8fd430b5b38048598a5b0cf093c326ae0794b6d
martynek <xmartynsx@gmail.com>
2018-10-15 12:48:23 +0200

    baza

--- baza/gus/bazagus.sql
+++ baza/gus/bazagus.sql
@@ -17,7 +17,7 @@ CREATE TABLE miasta (
  --sqlite>.quit - wyjscie z interpretera
  
  CREATE TABLE dane_demograficzne (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY,
     liczba_mieszkancow INTEGER,
     liczba_kobiet INTEGER,
     grupa_wiekowa TEXT(15),
